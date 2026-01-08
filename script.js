f5d3486768a94f4ddf1ff47d32c59e4650f97639
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-08 15:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit",function(e){
+	e.preventDefault();
+
+const form = e.target;
+
+const firstName = form["First Name"].value;
+const lastName = form["Last Name"].value;
+const phone = form["Phone Number"].value;
+const email = form["Email ID"].value;
+
+	alert(
+		"First Name: " + firstName + " " +
+		"Last Name: " + lastName + " " +
+		"Phone Number: " + phone + " " +
+		"Email ID: " + email
+	);
+});
